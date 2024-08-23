fcb57ee3d49baffbf8cc8dab23dc6d24264a1e14
Dario Tarasco <dario.tarasco@remira.com>
2024-08-23 16:33:35 +0200

    to do fix for edit pharagraph modify

--- src/MadSadGlad/CardSection.jsx
+++ src/MadSadGlad/CardSection.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { IconButton, Tooltip } from '@mui/material';
-import {
-  Check as CheckIcon,
-  Close as CloseIcon,
-} from '@mui/icons-material';
+import { IconButton, Tooltip, Button } from '@mui/material';
+import { Check as CheckIcon, Close as CloseIcon, Edit as EditIcon, Save as SaveIcon } from '@mui/icons-material';
 import './CardSection.css';
 import AutoSizeTextArea from './AutoSizeText';
+import './AutoSizeText.css';
 
-function CardSection() {
+function CardSection({ onPublish }) {
   const [text, setText] = useState('');
-  const [cards, setCards] = useState([]); // Stato per gestire i post-it creati
+  const [cards, setCards] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
   const maxCharacters = 140;
 
   const handleChange = (event) => {
@@ -19,6 +18,13 @@ function CardSection() {
     }
   };
 
+  const handleCardChange = (event, index) => {
+    const newText = event.target.value;
+    const updatedCards = [...cards];
+    updatedCards[index] = newText;
+    setCards(updatedCards);
+  };
+
   const handleSave = () => {
     if (text.trim()) {
       setCards([...cards, text]);
@@ -26,9 +32,49 @@ function CardSection() {
     }
   };
 
+  const handleEdit = (index) => {
+    setEditIndex(index);
+  };
+
+  const handleSaveEdit = () => {
+    setEditIndex(null);
+  };
+
   const handleDelete = (index) => {
     const newCards = cards.filter((_, i) => i !== index);
     setCards(newCards);
+    if (editIndex === index) {
+      setEditIndex(null);
+    }
+  };
+
+  const handlePublishAll = () => {
+    if (cards.length > 0) {
+      onPublish(
+        cards.map((cardText, index) => (
+          <div key={index}>
+            <div className="card-content">
+              <div className="card-description-container">
+                <AutoSizeTextArea
+                  value={cardText}
+                  readOnly={true}
+                  minFontSize={8}
+                  maxFontSize={16}
+                  stepGranularity={2}
+                  style={{
+                    width: '100%',
+                    textAlign: 'center',
+                    border: 'none',
+                    backgroundColor: 'transparent',
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ))
+      );
+      setCards([]);  // Optionally clear cards after publishing
+    }
   };
 
   return (
@@ -36,11 +82,38 @@ function CardSection() {
       <div className="cards-container">
         {cards.map((cardText, index) => (
           <div className="card" key={index}>
-            <div className="card-content">
+            <div className="card-content edit">
               <div className="card-description-container">
-                <p>{cardText}</p>
+                <AutoSizeTextArea
+                  value={cardText}
+                  onChange={(event) => handleCardChange(event, index)}// questo devo portarlo anche di là
+                  readOnly={editIndex !== index}
+                  minFontSize={8}
+                  maxFontSize={16}
+                  stepGranularity={2}
+                  style={{
+                    width: '100%',
+                    height: '100px',
+                    textAlign: 'center',
+                    border: 'none',
+                    backgroundColor: 'transparent',
+                  }}
+                />
               </div>
               <div className="buttons">
+                {editIndex === index ? (
+                  <Tooltip title="Save">
+                    <IconButton color="success" onClick={handleSaveEdit}>
+                      <SaveIcon />
+                    </IconButton>
+                  </Tooltip>
+                ) : (
+                  <Tooltip title="Edit">
+                    <IconButton color="default" onClick={() => handleEdit(index)}>
+                      <EditIcon />
+                    </IconButton>
+                  </Tooltip>
+                )}
                 <Tooltip title="Delete">
                   <IconButton color="error" onClick={() => handleDelete(index)}>
                     <CloseIcon />
@@ -50,7 +123,6 @@ function CardSection() {
             </div>
           </div>
         ))}
-
         <div className="card new-card">
           <div className="card-content edit">
             <div className="card-description-container">
@@ -71,12 +143,12 @@ function CardSection() {
               />
             </div>
             <div className="buttons">
-              <Tooltip title="Save">
+              <Tooltip>
                 <IconButton color="success" onClick={handleSave}>
                   <CheckIcon />
                 </IconButton>
               </Tooltip>
-              <Tooltip title="Cancel">
+              <Tooltip>
                 <IconButton color="error" onClick={() => setText('')}>
                   <CloseIcon />
                 </IconButton>
@@ -86,6 +158,9 @@ function CardSection() {
           </div>
         </div>
       </div>
+      <Button variant="contained" onClick={handlePublishAll} style={{ marginTop: '10px' }}>
+        Publish All
+      </Button>
     </div>
   );
 }
@@ -93,4 +168,5 @@ function CardSection() {
 export default CardSection;
 
 
-//Cambiare paragrafo e metterlo in textarea
+// tasto modifica nel new card
+//devo il tasto pubblica nel comtainer che ,mi permette di buttare la card nel container sopra
--- src/MadSadGlad/MadSadGladPage.jsx
+++ src/MadSadGlad/MadSadGladPage.jsx
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MadSadGladPage.css';
-import { Button } from '@mui/material';
+import { Button, IconButton, Tooltip } from '@mui/material';
 import CardSection from './CardSection';
-
-function EditableParagraph({ initialText }) {
-  const paragraphRef = React.useRef(null);
-
-  React.useEffect(() => {
-    if (paragraphRef.current) {
-      paragraphRef.current.innerText = initialText;
-    }
-  }, [initialText]);
-
-  return (
-    <p
-      contentEditable
-      suppressContentEditableWarning={true}
-      spellCheck={false}  // Disabilita il controllo ortografico
-      className="editable-paragraph"
-      ref={paragraphRef}
-    />
-  );
-}
+import { Check as CheckIcon, Close as CloseIcon, Edit as EditIcon, Save as SaveIcon } from '@mui/icons-material';
 
 function FirstPage() {
+  const [madCards, setMadCards] = useState([]);
+  const [sadCards, setSadCards] = useState([]);
+  const [gladCards, setGladCards] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handlePublish = (cards, column) => {
+    switch (column) {
+      case 'mad':
+        setMadCards((prevCards) => [...prevCards, ...cards]);
+        break;
+      case 'sad':
+        setSadCards((prevCards) => [...prevCards, ...cards]);
+        break;
+      case 'glad':
+        setGladCards((prevCards) => [...prevCards, ...cards]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleEdit = (index, column) => {
+    setEditIndex({ index, column });
+  };
+
+  const handleSaveEdit = () => {
+    setEditIndex(null);
+  };
+
+  const handleDelete = (index, column) => {
+    switch (column) {
+      case 'mad':
+        setMadCards((prevCards) => prevCards.filter((_, i) => i !== index));
+        break;
+      case 'sad':
+        setSadCards((prevCards) => prevCards.filter((_, i) => i !== index));
+        break;
+      case 'glad':
+        setGladCards((prevCards) => prevCards.filter((_, i) => i !== index));
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="App">
       <header className="headerMadSadGlad">
@@ -44,29 +70,120 @@ function FirstPage() {
       <div className="content">
         <div className="column mad">
           <h2>Mad</h2>
-          <EditableParagraph
-            initialText="What has driven you mad? You might focus on issues, time wasters, unpleasant surprises, etc."
-          />
+          <div className="editable-paragraph-container">
+            <div className="editable-paragraph">
+              {madCards.length === 0 ? (
+                <p
+                  contentEditable
+                  suppressContentEditableWarning={true}
+                  spellCheck={false}
+                  className="editable-paragraph-text"
+                >
+                  What has driven you mad? You might focus on issues, time wasters, unpleasant surprises, etc.
+                </p>
+              ) : (
+                madCards.map((card, index) => (
+                  <div key={index} className="editable-card">
+                    {card}
+                    <div className="card-buttons">
+                      <Tooltip title="Edit">
+                        <IconButton color="default" onClick={() => handleEdit(index, 'mad')}>
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Delete">
+                        <IconButton color="error" onClick={() => handleDelete(index, 'mad')}>
+                          <CloseIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
           <div className="post-it-container">
-            <CardSection />
+            <CardSection onPublish={(cards) => handlePublish(cards, 'mad')} />
           </div>
         </div>
+        
+        {/* Repeat similar blocks for 'Sad' and 'Glad' columns */}
+        
         <div className="column sad">
           <h2>Sad</h2>
-          <EditableParagraph
-            initialText="What has made you feel sad? You might focus on failures, issues inside the team, etc."
-          />
+          <div className="editable-paragraph-container">
+            <div className="editable-paragraph">
+              {sadCards.length === 0 ? (
+                <p
+                  contentEditable
+                  suppressContentEditableWarning={true}
+                  spellCheck={false}
+                  className="editable-paragraph-text"
+                >
+                  What has driven you sad? You might focus on disappointments, challenges, etc.
+                </p>
+              ) : (
+                sadCards.map((card, index) => (
+                  <div key={index} className="editable-card">
+                    {card}
+                    <div className="card-buttons">
+                      <Tooltip title="Edit">
+                        <IconButton color="default" onClick={() => handleEdit(index, 'sad')}>
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Delete">
+                        <IconButton color="error" onClick={() => handleDelete(index, 'sad')}>
+                          <CloseIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
           <div className="post-it-container">
-            <CardSection />
+            <CardSection onPublish={(cards) => handlePublish(cards, 'sad')} />
           </div>
         </div>
+
         <div className="column glad">
           <h2>Glad</h2>
-          <EditableParagraph
-            initialText="What has made you feel positive? You might focus on goals achieved, successes, events, etc."
-          />
+          <div className="editable-paragraph-container">
+            <div className="editable-paragraph">
+              {gladCards.length === 0 ? (
+                <p
+                  contentEditable
+                  suppressContentEditableWarning={true}
+                  spellCheck={false}
+                  className="editable-paragraph-text"
+                >
+                  What has made you glad? You might focus on successes, positive experiences, etc.
+                </p>
+              ) : (
+                gladCards.map((card, index) => (
+                  <div key={index} className="editable-card">
+                    {card}
+                    <div className="card-buttons">
+                      <Tooltip title="Edit">
+                        <IconButton color="default" onClick={() => handleEdit(index, 'glad')}>
+                          <EditIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Delete">
+                        <IconButton color="error" onClick={() => handleDelete(index, 'glad')}>
+                          <CloseIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
           <div className="post-it-container">
-            <CardSection />
+            <CardSection onPublish={(cards) => handlePublish(cards, 'glad')} />
           </div>
         </div>
       </div>
